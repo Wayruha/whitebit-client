3048b0e0a7d710abf4978f91867a82742cbc0696
RomanDiachuk <rwdiachuk@gmail.com>
2024-01-01 16:20:32 +0100

    fix Withdraw request

--- src/main/java/trade/wayruha/whitebit/dto/request/WithdrawRequest.java
+++ src/main/java/trade/wayruha/whitebit/dto/request/WithdrawRequest.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 @Data
 public class WithdrawRequest {
   private String ticker;
-  private BigDecimal amount;
+  private String amount;
   private String address;
   private String memo;
   private String uniqueId;
